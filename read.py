8b6fa71fc984ba485924b18bd6e99a1b73d3a894
San_ggwp <mike6010550105@icloud.com>
2020-12-08 20:17:27 +0800

    12/8

--- read.py
+++ read.py
@@ -7,3 +7,24 @@ def read_data_fox(filename):
     matrix_list = matrix.iloc[:,:].values
 
     return matrix_list
+
+def read_data_ori(filename):
+    df = pd.read_csv(filename,
+                   sep='\t', names=['user','item','rate','time'], engine='python', encoding='latin-1')
+    matrix = df.pivot(index='user', columns='item', values='rate')
+    matrix.fillna(0, inplace=True)
+    matrix_list = matrix.iloc[:,:].values
+
+    return matrix_list
+
+path = "result/mon1.csv"
+data = read_data_fox(path)
+print(data.shape)
+path_ = 'result/ori.csv'
+dataset = read_data_ori(path_)
+print(dataset.shape)
+
+# dataset = pd.read_csv('ml-100k/u.data', delimiter='\t', header=None, names=['userID', 'movieID', 'rating', 'timestemp'])
+
+# data_ = dataset.to_numpy()
+# print(data_)
